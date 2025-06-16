9d659883cd114c6b14b0bd7a9d9fe95b8d4c852b
Sabbir-Hossain-00 <ms7398037@gmail.com>
2025-06-16 22:37:48 +0600

    added dark theme at event details page

--- src/pages/Event-Details/EventDetails.jsx
+++ src/pages/Event-Details/EventDetails.jsx
@@ -7,7 +7,7 @@ import moment from "moment";
 import { Fade } from "react-awesome-reveal";
 
 export const EventDetails = ()=>{
-    const {user} = use(AuthContext)
+    const {user , isDark} = use(AuthContext)
     const navigate = useNavigate();
     const axiousSecure = useAxiousSecure();
     const {id} = useParams();
@@ -65,7 +65,7 @@ export const EventDetails = ()=>{
     return(
         <section className="pt-30 pb-10 container mx-auto px-3 md:px-6 lg:px-8 xl:px-20">
           <Fade>
-              <div className="px-3 md:px-20 md:py-20 py-4 card lg:card-side bg-gradient-to-l from-white to-[#eee5f1] border-2 rounded-2xl border-white">
+              <div className={`px-3 md:px-20 md:py-20 py-4 card lg:card-side border-2 rounded-2xl  ${isDark ? "bg-gradient-to-l from-[#02272b] to-[#000000] border-black":" bg-gradient-to-l from-white to-[#eee5f1] border-white"}`}>
             <figure className="w-full">
               <img
                 src={ThumbPhoto}
@@ -74,12 +74,12 @@ export const EventDetails = ()=>{
             </figure>
             <div className="card-body">
               <h2 className="card-title lg:text-3xl text-2xl">{eventTitle}</h2>
-              <p className=""><span className="font-medium">Event Type :</span> <span className="bg-fuchsia-100 text-xs py-2 px-4 rounded-full">{eventTitle}</span></p>
+              <p className=""><span className="font-medium">Event Type :</span> <span className={` text-xs py-2 px-4 rounded-full ${isDark ? "bg-[#02272b]":"bg-fuchsia-100"}`}>{eventTitle}</span></p>
               <p className=""><span className="font-medium">Event Date :</span> {formattedDate}</p>
               <p className=""><span className="font-medium">Location :</span> {eventLocation}</p>
               <p className=""><span className="font-medium">Description :</span> {description}</p>
               <div className="card-actions ">
-                <button onClick={handleJoinEvent} className="btn bg-[#e4c1f9]">Join Event</button>
+                <button className={`btn shadow-none border-none ${isDark? "bg-[#006d77] text-white" : "bg-[#e4c1f9] text-black"}`}>Join Event</button>
               </div>
             </div>
           </div>
