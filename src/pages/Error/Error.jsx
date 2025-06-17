92b0226a90a046466cf714a99f852712c55b1e74
Sabbir-Hossain-00 <ms7398037@gmail.com>
2025-06-17 13:24:59 +0600

    final commit after second check

--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -1,12 +1,15 @@
+import { use } from "react";
 import { useNavigate } from "react-router"
+import { AuthContext } from "../../context/AuthContext";
 
 export const Error = ()=>{
     const navigate = useNavigate();
+    const {isDark} = use(AuthContext)
     return(
         <>
            <div className="flex flex-col justify-center items-center">
               <img src="https://i.ibb.co/7xPt3K3D/404.gif" alt="" />
-              <button onClick={()=> navigate("/")} className="btn bg-[#e4c1f9] border-none">Go Back To Home</button>
+              <button onClick={()=> navigate("/")} className={`btn shadow-none border-none ${isDark ?"bg-[#006d77] text-white" :"bg-[#e4c1f9] text-black"}`}>Go Back To Home</button>
            </div>
         </>
     )
--- src/pages/Manage-Events/EmptyManageEvent.jsx
+++ src/pages/Manage-Events/EmptyManageEvent.jsx
@@ -1,11 +1,14 @@
+import { use } from "react"
 import { Link } from "react-router"
+import { AuthContext } from "../../context/AuthContext"
 
 export const EmptyManageEvent = ()=>{
+    const {isDark} =use(AuthContext)
     return(
         <section className="flex flex-col justify-center items-center min-h-screen  gap-4 p-3 ">
-            <h1 className="md:text-3xl text-2xl font-medium text-[#d79ef8] text-center">You haven't create any event yet</h1>
+            <h1 className={`md:text-3xl text-2xl font-medium  text-center ${isDark ? "text-[#006d77]":"text-[#d79ef8]"}`}>You haven't create any event yet</h1>
             <p className="text-lg font-medium">please create an event </p>
-            <Link to="/create-event" className="btn bg-[#e4c1f9]">Create an Event</Link>
+            <Link to="/create-event" className={`btn w-full shadow-none border-none ${isDark? "bg-[#006d77] text-white": "bg-[#e4c1f9] text-black"}`}>Create an Event</Link>
         </section>
     )
 }
