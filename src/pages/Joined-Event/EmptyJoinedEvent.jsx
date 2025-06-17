6ca02a240a487304a1192ec2c9b54a1a2532aeed
Sabbir-Hossain-00 <ms7398037@gmail.com>
2025-06-17 13:31:17 +0600

    final commit after third check

--- src/pages/Joined-Event/EmptyJoinedEvent.jsx
+++ src/pages/Joined-Event/EmptyJoinedEvent.jsx
@@ -8,7 +8,7 @@ export const EmptyJoinedEvent = ()=>{
         <section className="flex flex-col justify-center items-center min-h-screen gap-4 p-3">
             <h1 className={`md:text-3xl text-2xl font-medium  text-center ${isDark ? "text-[#006d77]":"text-[#d79ef8]"}`}>You haven't joined any event yet</h1>
             <p className="text-lg font-medium">please join an event </p>
-            <Link to="/upcoming-events" className={`btn w-full shadow-none border-none ${isDark? "bg-[#006d77] text-white": "bg-[#e4c1f9] text-black"}`}>Join Now</Link>
+            <Link to="/upcoming-events" className={`btn shadow-none border-none ${isDark? "bg-[#006d77] text-white": "bg-[#e4c1f9] text-black"}`}>Join Now</Link>
         </section>
     )
 }
--- src/pages/Manage-Events/EmptyManageEvent.jsx
+++ src/pages/Manage-Events/EmptyManageEvent.jsx
@@ -8,7 +8,7 @@ export const EmptyManageEvent = ()=>{
         <section className="flex flex-col justify-center items-center min-h-screen  gap-4 p-3 ">
             <h1 className={`md:text-3xl text-2xl font-medium  text-center ${isDark ? "text-[#006d77]":"text-[#d79ef8]"}`}>You haven't create any event yet</h1>
             <p className="text-lg font-medium">please create an event </p>
-            <Link to="/create-event" className={`btn w-full shadow-none border-none ${isDark? "bg-[#006d77] text-white": "bg-[#e4c1f9] text-black"}`}>Create an Event</Link>
+            <Link to="/create-event" className={`btn shadow-none border-none ${isDark? "bg-[#006d77] text-white": "bg-[#e4c1f9] text-black"}`}>Create an Event</Link>
         </section>
     )
 }
